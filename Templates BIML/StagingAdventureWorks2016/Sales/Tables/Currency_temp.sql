0be04e1989a4e7d2b0d95e269b0578875406fc98
abravikov <33125286+abravikov@users.noreply.github.com>
2018-05-10 19:08:01 +0200

    Start of incremental project

---
+++ Templates BIML/StagingAdventureWorks2016/Sales/Tables/Currency_temp.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [Sales].[Currency_temp] (
+    [CurrencyCode] NCHAR (3)     NOT NULL,
+    [Name]         NVARCHAR (50) NOT NULL,
+    [ModifiedDate] DATETIME      NOT NULL,
+	[Staging Load Date] DATETIME     NOT NULL,
+    CONSTRAINT [PK_Currency_temp_CurrencyCode] PRIMARY KEY CLUSTERED ([CurrencyCode] ASC)
+);
+
---
+++ Templates BIML/StagingAdventureWorks2016/Sales/Tables/Customer_temp.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [Sales].[Customer_temp] (
+    [CustomerID]    INT            NOT NULL,
+    [PersonID]      INT              NULL,
+    [StoreID]       INT              NULL,
+    [TerritoryID]   INT              NULL,
+    [AccountNumber] VARCHAR (10)     NULL,
+    [rowguid]       UNIQUEIDENTIFIER NOT NULL,
+    [ModifiedDate]  DATETIME         NOT NULL,
+	[Staging Load Date] DATETIME     NOT NULL,
+    CONSTRAINT [PK_Customer_temp_CustomerID] PRIMARY KEY CLUSTERED ([CustomerID] ASC)
+);
+
