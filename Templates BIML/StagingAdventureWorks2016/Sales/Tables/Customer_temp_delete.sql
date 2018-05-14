60683d73e9b8dda7b003661f34cd3e1387b8be32
alebra <alexander.bravikov@spv.no>
2018-05-14 18:13:34 +0200

    Adding SWITCH if it the first load. No need to do this for big tables.

---
+++ Templates BIML/StagingAdventureWorks2016/Sales/Tables/Customer_temp_delete.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [SalesStg].[Customer_temp_delete] (
+    [CustomerID]    INT            NOT NULL,
+    CONSTRAINT [PK_Customer_temp__delete_CustomerID] PRIMARY KEY CLUSTERED ([CustomerID] ASC)
+);
+
